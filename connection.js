6ed355b2ff4e41727f6af6b7d519c67abe2cfaf9
Jordan <knightworks@pm.me>
2020-04-04 15:34:34 -0500

    test

--- connection.js
+++ connection.js
@@ -1,9 +1,10 @@
 const { env } = require('@frontierjs/toolbelt')
+console.log(env.get('DB'))
 
 const opts = {
   //TODO: fix the options
-    memory: env.get('DB_USE_MEM') === 'true' ? true : false,
-    verbose: env.get('DB_USE_LOG') ? console.log : null
+  memory: env.get('DB_USE_MEM') === 'true' ? true : false,
+  verbose: env.get('DB_USE_LOG') ? console.log : null
 }
 
 const Connection = require('better-sqlite3')(env.get('DB'), opts)

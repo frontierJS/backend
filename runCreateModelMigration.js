4983803a1aedc43c4b60d2d18acb22ddcd87fa05
Jordan <knightworks@pm.me>
2020-07-24 21:29:28 -0500

    fix

--- runCreateModelMigration.js
+++ runCreateModelMigration.js
@@ -1,35 +1,35 @@
-const { env, logger, args } = require('@frontierjs/toolbelt')
+import { env, logger, args } from '@frontierjs/backend'
 const log = logger
 //const argv = require('@frontierjs/toolbelt/lib/args')
-const fs = require('fs')
-const path = require('path')
+import fs from 'fs'
+import path from 'path'
 
 //This should be in frontier
-let tryIt = function(fn, ...arguments) {
-    console.log(arguments)
-    try {
-        return fn(...arguments)
-    } catch(err) {
-        //pretty log it
-        log.err(err)
-    }
+let tryIt = function (fn, ...arguments) {
+  console.log(arguments)
+  try {
+    return fn(...arguments)
+  } catch (err) {
+    //pretty log it
+    log.err(err)
+  }
 }
 
 let modelPath = (() => {
-      let p = path.resolve(process.cwd() + '/src/Models')
-    return p.slice(0, p.lastIndexOf('/')) + '/Models/'
+  let p = path.resolve(process.cwd() + '/src/Models')
+  return p.slice(0, p.lastIndexOf('/')) + '/Models/'
 })()
 
 let modelFiles = []
 if (args.models) {
-   modelFiles = args.models.split(',')
+  modelFiles = args.models.split(',')
 } else {
-     modelFiles = tryIt(fs.readdirSync, modelPath)
+  modelFiles = tryIt(fs.readdirSync, modelPath)
 }
 
-modelFiles.forEach(file => {
-    let model = require(modelPath + file)
+modelFiles.forEach((file) => {
+  let model = require(modelPath + file)
   console.log(model)
-    let stmt = model.createTable()
-    log.info({stmt})
+  let stmt = model.createTable()
+  log.info({ stmt })
 })

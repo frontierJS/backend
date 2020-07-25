583f49de95557ef6e96a92d21d2726dbc9d13021
Jordan <knightworks@pm.me>
2020-07-24 23:17:25 -0500

    test

--- index.js
+++ index.js
@@ -4,4 +4,5 @@ import BaseModel from './BaseModel.js'
 import Database from './Database.js'
 
 let env = Toolbelt.env
-export { Model, BaseModel, Database, Toolbelt, env }
+let backend = { Model, BaseModel, Database, Toolbelt, env }
+export default backend
